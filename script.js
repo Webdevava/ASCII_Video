505976cd03dfff7cae02fa905464a24b080b8e39
Ankur Auti <115859079+Webdevava@users.noreply.github.com>
2023-08-22 13:21:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,9 +22,36 @@ function setup() {
   asciiDiv = createDiv();
 }
 
+// function draw() {
+//   video.loadPixels();
+//   let asciiImage = "";
+//   for (let j = 0; j < video.height; j++) {
+//     for (let i = 0; i < video.width; i++) {
+//       const pixelIndex = (i + j * video.width) * 4;
+//       const r = video.pixels[pixelIndex + 0];
+//       const g = video.pixels[pixelIndex + 1];
+//       const b = video.pixels[pixelIndex + 2];
+//       const avg = (r + g + b) / 3;
+//       const len = density.length;
+//       const charIndex = floor(map(avg, 0, 255, 0, len));
+//       const c = density.charAt(charIndex);
+//       if (c == " ") asciiImage += "&nbsp;";
+//       else asciiImage += c;
+//     }
+//     asciiImage += '<br/>';
+//   }
+//   asciiDiv.html(asciiImage);
+// }
+
 function draw() {
   video.loadPixels();
   let asciiImage = "";
+  
+  // Flip the video horizontally
+  push(); // Save the current transformation state
+  translate(video.width, 0); // Move the origin to the right edge
+  scale(-1, 1); // Flip the video horizontally
+  
   for (let j = 0; j < video.height; j++) {
     for (let i = 0; i < video.width; i++) {
       const pixelIndex = (i + j * video.width) * 4;
@@ -40,5 +67,9 @@ function draw() {
     }
     asciiImage += '<br/>';
   }
+  
+  pop(); // Restore the previous transformation state
+  
   asciiDiv.html(asciiImage);
 }
+
